a9d00e5f762ddd9da1b84a791ed1d08c15f14fde
Johan Blomgren <johan@dryg.net>
2019-12-14 15:16:15 +0100

    nojimp

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import { jimpEvChange } from '@jimp/core';
 import Ajv from 'ajv';
 import bodyParser from 'body-parser';
 import express from 'express';
@@ -13,8 +12,8 @@ import { MongoHelper } from './server/mongo.helper';
 let client: MongoClient;
 
 MongoHelper.connect(config.DBServer).then((res) => {
-    client = res;
-    initDB(client);
+  client = res;
+  initDB(client);
 });
 
 const jsonParser = bodyParser.json();
@@ -27,67 +26,67 @@ srv.use('/:appName/functions/:functionName', [resolveFunction, handleFunction]);
 srv.use('/:appName/db/:collection/:id*?', [checkCollection]);
 
 function handleFunction(req: express.Request, res: express.Response) {
-    res.locals.function(req, res);
+  res.locals.function(req, res);
 }
 
 srv.post('/:appName/files', (req: express.Request, res: express.Response) => {
-    const storage = new GridFsStorage({
-        db: client.db(res.locals.appName),
-        file: (request, file) => {
-            return {
-                bucketName: 'storage',
-                filename: file.originalname,
-            };
-        }
-    });
+  const storage = new GridFsStorage({
+    db: client.db(res.locals.appName),
+    file: (request, file) => {
+      return {
+        bucketName: 'storage',
+        filename: file.originalname,
+      };
+    }
+  });
 
-    storage.on('connection', (db) => {
-        const upload = multer({
-            storage
-        }).single('file');
-        upload(req, res, (err: any) => {
-            if (err) {
-                console.log(err);
-                return res.send({ title: 'Uploaded Error', message: 'File could not be uploaded', error: err });
-            }
-            res.send({ title: 'Uploaded', message: `File has been ${req.file.id} uploaded!` });
-        });
+  storage.on('connection', (db) => {
+    const upload = multer({
+      storage
+    }).single('file');
+    upload(req, res, (err: any) => {
+      if (err) {
+        console.log(err);
+        return res.send({ title: 'Uploaded Error', message: 'File could not be uploaded', error: err });
+      }
+      res.send({ title: 'Uploaded', message: `File has been ${req.file.id} uploaded!` });
     });
+  });
 });
 
 srv.post('/:appName/db/:collection', (req: express.Request, res: express.Response) => {
-    const db = client.db(req.params.appName);
-    db.collection(req.params.collection, (error: any, collection: any) => {
-        collection.insertOne(req.body, (err: any, dbresp: any) => {
-            if (err) {
-                validate(req, res, err);
-            } else {
-                res.send(dbresp.ops[0]);
-            }
-        });
+  const db = client.db(req.params.appName);
+  db.collection(req.params.collection, (error: any, collection: any) => {
+    collection.insertOne(req.body, (err: any, dbresp: any) => {
+      if (err) {
+        validate(req, res, err);
+      } else {
+        res.send(dbresp.ops[0]);
+      }
     });
+  });
 });
 
 async function validate(req: express.Request, res: express.Response, err: any) {
-    res.status(400);
-    const db = client.db(req.params.appName);
-    const collinfo: any = await db.listCollections({ name: req.params.collection }).next();
-    const schema = collinfo.options.validator;
-    const data = req.body;
-    const ajv = new Ajv();
-    const valid = schema ? ajv.validate(schema.$jsonSchema, data) : true;
-    if (!valid) {
-        let hint = 'Check your data, (or schema if this is early in development)';
-        if (ajv.errors[0].keyword === 'additionalProperties') {
-            hint = 'when additionalProperties = false, add "_id: { bsonType: \'objectId\' }" to the schema';
-        }
-        res.send({ error: err, validationError: ajv.errors, schema, hint });
-    } else {
-        res.send({ error: err });
+  res.status(400);
+  const db = client.db(req.params.appName);
+  const collinfo: any = await db.listCollections({ name: req.params.collection }).next();
+  const schema = collinfo.options.validator;
+  const data = req.body;
+  const ajv = new Ajv();
+  const valid = schema ? ajv.validate(schema.$jsonSchema, data) : true;
+  if (!valid) {
+    let hint = 'Check your data, (or schema if this is early in development)';
+    if (ajv.errors[0].keyword === 'additionalProperties') {
+      hint = 'when additionalProperties = false, add "_id: { bsonType: \'objectId\' }" to the schema';
     }
+    res.send({ error: err, validationError: ajv.errors, schema, hint });
+  } else {
+    res.send({ error: err });
+  }
 }
 
 // start the express server
 srv.listen(port, () => {
-    console.log(`server started at http://localhost:${port}`);
+  console.log(`server started at http://localhost:${port}`);
 });
--- src/server/middleware.ts
+++ src/server/middleware.ts
@@ -2,38 +2,38 @@ import express from 'express';
 import apps from '../apps';
 
 const resolveApp = (req: express.Request, res: express.Response, next: () => void) => {
-	const appName = req.params.appName;
-	console.log('resolveApp', req.params);
-	if (!apps[appName]) {
-		res.send(`No app named ${appName} !!!`);
-		return;
+  const appName = req.params.appName;
+  console.log('resolveApp', req.params);
+  if (!apps[appName]) {
+    res.send(`No app named ${appName} !!!`);
+    return;
 
-	}
-	res.locals.appName = appName;
-	res.locals.app = apps[appName];
-	next();
+  }
+  res.locals.appName = appName;
+  res.locals.app = apps[appName];
+  next();
 };
 
 const checkCollection = (req: express.Request, res: express.Response, next: () => void) => {
-	const collection = req.params.collection;
-	if (!res.locals.app.collections.find((col: any) => col.name === collection)) {
-		res.send(`No col named ${collection} !!!`);
-		return;
-	}
-	next();
+  const collection = req.params.collection;
+  if (!res.locals.app.collections.find((col: any) => col.name === collection)) {
+    res.send(`No col named ${collection} !!!`);
+    return;
+  }
+  next();
 };
 
 const resolveFunction = (req: express.Request, res: express.Response, next: () => void) => {
-	const app = res.locals.app;
-	console.log('resolveFun', app);
-	const functionName = req.params.functionName;
-	if (!app[functionName]) {
-		res.send(`No fun named ${functionName} !!!`);
-		return;
-	} else {
-		res.locals.function = app[functionName];
-	}
-	next();
+  const app = res.locals.app;
+  console.log('resolveFun', app);
+  const functionName = req.params.functionName;
+  if (!app[functionName]) {
+    res.send(`No fun named ${functionName} !!!`);
+    return;
+  } else {
+    res.locals.function = app[functionName];
+  }
+  next();
 };
 
 export { resolveApp, resolveFunction, checkCollection };
