1069b98b25c1864eed0ade4272dc51cca7a67f4c
Johan Blomgren <jblb@jblb-mac-HH2F.int.svenskaspel.se>
2019-12-18 09:41:50 +0100

    refactor

--- src/apps/tvinnaren/schemas.ts
+++ src/apps/tvinnaren/schemas.ts
@@ -1,27 +1,27 @@
 import { IJsonSchema } from '../../models/IJsonSchema';
 
 const tvinnarenCase: IJsonSchema = {
-	bsonType: 'object',
-	additionalProperties: false,
-	properties: {
-		_id: { bsonType: 'objectId' },
-		caseId: { bsonType: 'int' },
-		status: { enum: ['nytt', 'stängt'] },
-		subcases: {
-			bsonType: 'array',
-			items:
-			{
-				bsonType: 'object',
-				additionalProperties: false,
-				required: ['status'],
-				properties: {
-					status: { enum: ['nytt', 'arbetar'] }
-				}
-			}
-		},
-		title: { bsonType: 'string' }
-	},
-	required: ['caseId', 'title']
+  bsonType: 'object',
+  additionalProperties: false,
+  properties: {
+    _id: { bsonType: 'objectId' },
+    caseId: { bsonType: 'int' },
+    status: { enum: ['nytt', 'stängt'] },
+    subcases: {
+      bsonType: 'array',
+      items:
+      {
+        bsonType: 'object',
+        additionalProperties: false,
+        required: ['status'],
+        properties: {
+          status: { enum: ['nytt', 'arbetar'] }
+        }
+      }
+    },
+    title: { bsonType: 'string' }
+  },
+  required: ['caseId', 'title']
 };
 
 export { tvinnarenCase };
